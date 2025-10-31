272af670dab7d768afa8e5d9e2ed124b72aa5920
Prakash Thapa <prakashthapa617@gmail.com>
2025-10-31 17:03:40 +0545

    Deploy to nurseicon.com.np

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=280),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
